2930e4fabe51881784f44219f0362c98c5d072e2
Simone Giovo <sgiovo@e3r9p5.42roma.it>
2021-05-17 14:53:35 +0200

    lista_funzioni_aggiornata

--- IteratorList.hpp
+++ IteratorList.hpp
@@ -4,53 +4,53 @@
 
 namespace ft
 {
-template <class T> class listIterator
-{
-	private:
-		Node<T>	*curr;
-	public:
-		typedef T				value_type;
-		typedef std::ptrdiff_t 	difference_type;
-		typedef T*				pointer;
-		typedef T&				reference;
-		listIterator() {};
-		listIterator(listIterator const &copy) {};
+	template <class T> class listIterator
+	{
+		private:
+			Node<T>	*curr;
+		public:
+			typedef T				value_type;
+			typedef std::ptrdiff_t 	difference_type;
+			typedef T*				pointer;
+			typedef T&				reference;
+			listIterator() {};
+			listIterator(listIterator const &copy) {};
 
-		typedef listIterator<T> iterator;
+			typedef listIterator<T> iterator;
 
-		T			&operator * ()
-		{
-			return this->curr->value;
-		}
+			T			&operator * ()
+			{
+				return this->curr->value;
+			}
 
-		iterator	&operator ++ ()
-		{
-			this->curr = this->curr->next;
-			return this->curr->prev;
-		}
+			iterator	&operator ++ ()
+			{
+				this->curr = this->curr->next;
+				return this->curr->prev;
+			}
 
-		iterator	&operator ++ (int)
-		{
-			this->curr = this->curr->next;
-			return this->curr;
-		}
+			iterator	&operator ++ (int)
+			{
+				this->curr = this->curr->next;
+				return this->curr;
+			}
 
-		iterator	&operator -- ()
-		{
-			listIterator<T>::iterator temp = this;
-			this->curr = this->curr->prev;
-			return this->curr;
-		}
+			iterator	&operator -- ()
+			{
+				listIterator<T>::iterator temp = this;
+				this->curr = this->curr->prev;
+				return this->curr;
+			}
 
-		iterator	&operator -- (int)
-		{
-			this->curr = this->curr->prev;
-			return this->curr;
-		}
+			iterator	&operator -- (int)
+			{
+				this->curr = this->curr->prev;
+				return this->curr;
+			}
 
-		T			*operator -> ()
-		{
-			return &this->curr->value;
-		}
-    };
+			T			*operator -> ()
+			{
+				return &this->curr->value;
+			}
+	};
 }
--- List.hpp
+++ List.hpp
@@ -6,7 +6,7 @@
 /*   By: sgiovo <sgiovo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 14:56:49 by sgiovo            #+#    #+#             */
-/*   Updated: 2021/05/17 14:07:47 by sgiovo           ###   ########.fr       */
+/*   Updated: 2021/05/17 14:51:28 by sgiovo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,75 @@ namespace ft
 		list(/* args */) {};
 		~list() {};
 
+		//iter section
+		iterator begin();
+		const_iterator begin() const;
+		iterator end();
+		const_iterator end() const;
+		reverse_iterator rbegin();
+		const_reverse_iterator rbegin() const;
+    	reverse_iterator rend();
+		const_reverse_iterator rend() const;
 		
-		iterator begin (void)
-		{
-			return new listIterator<T>();
-		}
+		// Capacity
+		bool empty() const;
+		size_type size() const;
+		size_type max_size() const;
+		
+		//Elem access
+		reference front();
+		const_reference front() const;
+		reference back();
+		const_reference back() const;
+		
+		// Modifiers
+		template <class listIterator>
+  		void assign (InputIterator first, InputIterator last);							//range
+		
+		void assign (size_type n, const value_type& val);								//fill
+		void push_front (const value_type& val);
+		void pop_front();
+		void push_back (const value_type& val);
+		void pop_back();
+
+		template <class listIterator>
+		listIterator insert (iterator position, const value_type& val);
+	   	void insert (iterator position, size_type n, const value_type& val);
+		
+		template <class InputIterator>
+    	void insert (iterator position, InputIterator first, InputIterator last);
+		
+		template <class InputIterator>
+		Listiterator erase (iterator position);
+		template <class InputIterator>
+		Listiterator erase (iterator first, iterator last);
+		
+		void swap (list& x);
+		void resize (size_type n, value_type val = value_type());
+		void clear();
+
+		//operations
+		void splice (iterator position, list& x);										//entire list (1)	
+		void splice (iterator position, list& x, iterator i);							//single element (2)	
+		void splice (iterator position, list& x, iterator first, iterator last);		//element range (3)	
+		
+		void remove (const value_type& val);
+		template <class Predicate>
+		void remove_if (Predicate pred);
+		
+		void unique();
+		template <class BinaryPredicate>
+  		void unique (BinaryPredicate binary_pred);
+
+  		void merge (list& x);
+		template <class Compare>
+  		void merge (list& x, Compare comp);
+
+  		void sort();
+
+		template <class Compare>
+		void sort (Compare comp);
+		void reverse();
+
 	};
 } 
