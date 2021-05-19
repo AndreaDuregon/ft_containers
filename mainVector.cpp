03b98efe6a4399c71187e2d9d13945adeb86cbcb
Andrea Duregon <andreaduregon93@gmail.com>
2021-05-19 12:42:23 +0200

    iter vector to test

---
+++ mainVector.cpp
@@ -0,0 +1,17 @@
+#include "IteratorVector.hpp"
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <exception>
+/* COLORS */
+#define RED "\033[0;31m"
+#define	GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define OFF "\033[0m"
+/* END COLORS */
+
+
+int main(void)
+{
+
+}
