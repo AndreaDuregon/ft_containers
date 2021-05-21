c1ce1b57a4db2303201958f158d709f5348eb858
Andrea Duregon <aduregon@e3r9p7.42roma.it>
2021-05-21 18:52:18 +0200

    vector to finish

--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 11:41:56 by aduregon          #+#    #+#             */
-/*   Updated: 2021/05/21 12:54:19 by aduregon         ###   ########.fr       */
+/*   Updated: 2021/05/21 18:50:45 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,15 @@ namespace ft
 			this->vec_size = 0;
 		}
 
+		vector(vector<T> const &copy)
+		{
+			this->vec = alloc.allocate(copy.vec_capacity);
+			for (size_type i = 0; i < copy.vec_size; i++)
+				this->vec[i] = copy.vec[i];
+			this->vec_size = copy.vec_size;
+			this->vec_capacity = copy.vec_capacity;
+		}
+
 		vector(size_type n)
 		{
 			this->vec = alloc.allocate(n);
@@ -61,6 +70,11 @@ namespace ft
 			alloc.deallocate(this->vec, this->vec_capacity); 
 		}
 
+		vector& operator = (const vector& x)
+		{
+			vector tmp(x);
+			return &tmp;
+		}
 		// ITERATOR
 
 		iterator 				begin()
@@ -158,14 +172,6 @@ namespace ft
 			}
 		}
 
-		void		push_back (const value_type& val)
-		{
-			if (this->vec_size == this->vec_capacity - 1)
-				reserve(this->vec_capacity * 2);
-			this->vec[this->vec_size] = val;
-			this->vec_size++;
-		}
-
 		size_type	capacity() const
 		{
 			return this->vec_capacity;
@@ -203,5 +209,120 @@ namespace ft
 		{
 			return (this->vec[n]);
 		}
+
+		reference		front()
+		{
+			return *this->vec;
+		}
+
+		const_reference front() const
+		{
+			return *this->vec;
+		}
+
+		reference		back()
+		{
+			return 	this->vec[this->vec_size - 1];
+		}
+
+		const_reference back() const
+		{
+			return this->vec[this->vec_size - 1];
+		}
+
+		// MODIFIERS
+
+		template <class InputIterator>
+  		void		assign(InputIterator first, InputIterator last)
+		{
+			int i = 0;
+
+			this->clear();
+			while (first != last)
+			{
+				this->vec[i] = *first;
+				first++;
+				i++;
+			}
+		}
+
+		void 		assign(size_type n, const value_type& val)
+		{
+			int i = 0;
+
+			this->clear();
+			while (i < n)
+			{
+				this->vec[i] = val;
+				i++;
+			}
+		}
+		
+		void		push_back(const value_type& val)
+		{
+			if (this->vec_size == this->vec_capacity - 1)
+				reserve(this->vec_capacity * 2);
+			this->vec[this->vec_size] = val;
+			this->vec_size++;
+		}
+
+		void 		pop_back()
+		{
+			this->size--;
+		}
+
+		iterator 	insert(iterator position, const value_type& val)
+		{
+			int pos = 0;
+			iterator it1(this->begin());
+			while (it1 != position)
+			{
+				it1++;
+				pos++;
+			}
+			if (this->vec_capacity == this->vec_size)
+			{
+				this->reserve(this->vec_capacity * 2);
+				position = this->begin();
+				for (size_t i = 0; i < pos; i++)
+					position++;
+			}
+			iterator it(this->end());
+			int	i = this->vec_size;
+
+			while (it != position)
+			{
+				this->vec[i] = this->vec[i - 1];
+				it--;
+				i--;
+			}
+			this->vec[i] = val;
+			this->vec_size++;
+			return it;
+		}
+
+    	void 		insert(iterator position, size_type n, const value_type& val)
+		{
+			for (size_t i = 0; i < n; i++)
+				position = this->insert(position, val);
+		}
+
+		template <class InputIterator>
+    	void 		insert(iterator position, InputIterator first, InputIterator last)
+		{
+			while (first != last--)
+			{
+				position = this->insert(position, *(last));
+			}
+		}
+
+		void		clear()
+		{
+			alloc.deallocate(this->vec, this->vec_capacity);
+			this->vec = alloc.allocate(1);
+			this->vec_capacity = 1;
+			this->vec_size = 0;
+		}
+
 	};
 }
